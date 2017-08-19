48b915cc6c8ed0dfb93c414050c1e44de332f8b0
Arun Bahl <arun@bridge.ai>
2017-08-19 00:17:19 -0700

    Added additional dependencies in install script.

--- Tools/installfirst.sh
+++ Tools/installfirst.sh
@@ -2,4 +2,5 @@
 # Installs current Nodejs on a raspberry pi.
 # From http://thisdavej.com/beginners-guide-to-installing-node-js-on-a-raspberry-pi/
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-sudo apt install nodejs -y
+sudo apt install nodejs vim supervisor -y
+
